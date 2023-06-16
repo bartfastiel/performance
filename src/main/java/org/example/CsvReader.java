b659bf979b4df0a792ddbfe46e721ca3a4022ac2
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-16 22:16:52 +0200

    Process data in 10 parallel threads
    Benchmark      Mode  Cnt  Score   Error  Units
     CsvReader.run  avgt   50  0,095 ± 0,001   s/op

--- src/main/java/org/example/CsvReader.java
+++ src/main/java/org/example/CsvReader.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 @BenchmarkMode(Mode.AverageTime)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -21,6 +22,7 @@ public class CsvReader {
     public static final int FIRST_CHAR_OF_DAY_IN_DATE = 9;
     public static final int SECOND_CHAR_OF_DAY_IN_DATE = 10;
     public static final int NUMBER_OF_CHARS_OF_DATE_AND_BEHIND = 13;
+    public static final int NUMBER_OF_PARALLEL_THREADS = 10;
 
     public static void main(String[] args) throws IOException {
         org.openjdk.jmh.Main.main(new String[]{"org.example.CsvReader"});
@@ -30,7 +32,23 @@ public class CsvReader {
     @Benchmark
     public static String run() {
         byte[] data = readFile();
-        int[] personsPerBirthDay = createHistogram(data);
+
+        int approximateChunkSize = data.length / NUMBER_OF_PARALLEL_THREADS;
+        int[] personsPerBirthDay = IntStream.range(0, NUMBER_OF_PARALLEL_THREADS)
+                .parallel()
+                .map(chunk -> {
+                    int chunkStart = chunk * approximateChunkSize;
+                    while (chunkStart < data.length && data[chunkStart++] != '\n') ;
+                    return chunkStart;
+                })
+                .mapToObj(chunkStart -> createHistogram(data, chunkStart, approximateChunkSize))
+                .reduce((arr1, arr2) ->
+                        IntStream.range(0, Math.min(arr1.length, arr2.length))
+                                .map(i -> arr1[i] + arr2[i])
+                                .toArray()
+                )
+                .orElseThrow();
+
         return findBiggestParty(personsPerBirthDay);
     }
 
@@ -42,11 +60,14 @@ public class CsvReader {
         }
     }
 
-    private static int[] createHistogram(byte[] data) {
+    private static int[] createHistogram(byte[] data, int chunkStart, int approximateChunkSize) {
         int[] personsPerBirthDay = new int[12 * 31];
         int commaCount = 0;
-        for (int i = CHARACTERS_IN_TITLE + FIRST_CHARS_THAT_CONTAIN_ONE_COMMA_FOR_CERTAIN; i < data.length; i++) {
+        for (int i = chunkStart + CHARACTERS_IN_TITLE + FIRST_CHARS_THAT_CONTAIN_ONE_COMMA_FOR_CERTAIN; i < data.length; i++) {
             if (data[i] == '\n') {
+                if (i > chunkStart + approximateChunkSize) {
+                    break;
+                }
                 commaCount = 0;
                 i += FIRST_CHARS_THAT_CONTAIN_ONE_COMMA_FOR_CERTAIN;
             } else if (data[i] == ',' && commaCount++ == COMMAS_BEFORE_BIRTH_DATE) {

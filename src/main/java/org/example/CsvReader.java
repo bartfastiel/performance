72c5d4572c21e51ad2381fa5b70bce0732049424
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-16 21:06:57 +0200

    Merge methods, do not store records temporarily
    Benchmark      Mode  Cnt  Score   Error  Units
    CsvReader.run  avgt   50  0,673 ± 0,003   s/op

--- src/main/java/org/example/CsvReader.java
+++ src/main/java/org/example/CsvReader.java
@@ -5,11 +5,6 @@ import org.openjdk.jmh.annotations.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
@@ -21,113 +16,37 @@ public class CsvReader {
 
     public static void main(String[] args) throws IOException {
         org.openjdk.jmh.Main.main(new String[]{"org.example.CsvReader"});
-        //run();
+        // run();
     }
 
     @Benchmark
     public static String run() {
-        List<String> records = readCsvData();
-        String biggestParty = getMaximumPartyFriends(records);
-        return biggestParty;
-        // System.out.println("Biggest party is on " + biggestParty);
-    }
-
-    private static String getMaximumPartyFriends(List<String> records) {
         int[] personsPerBirthDay = new int[12 * 31];
 
         int maximumPartyFriends = 0;
         String winner = null;
-        for (String birthDate : records) {
 
-            int month = Integer.parseInt(birthDate.substring(5, 7));
-            int day = Integer.parseInt(birthDate.substring(8));
-            int calenderIndex = (month - 1) * 31 + (day - 1);
-
-            personsPerBirthDay[calenderIndex]++;
-
-            if (maximumPartyFriends < personsPerBirthDay[calenderIndex]) {
-                maximumPartyFriends = personsPerBirthDay[calenderIndex];
-                winner = birthDate.substring(5);
-            }
-        }
-        return winner;
-    }
-
-    private static List<String> readCsvData() {
-        return readShortenedFile();
-    }
-
-    /**
-     * Benchmark      Mode  Cnt  Score   Error  Units
-     * CsvReader.run  avgt   50  1,198 ± 0,018   s/op
-     */
-    private static List<List<String>> readCsvDataNioAllLines() {
-        try {
-            return Files.readAllLines(Path.of("people-2000000.csv"))
-                    .stream()
-                    .skip(1)
-                    .map(line -> Arrays.asList(line.split(",")))
-                    .toList();
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot read CSV", e);
-        }
-    }
-
-
-    /**
-     * Benchmark      Mode  Cnt  Score   Error  Units
-     * CsvReader.run  avgt   50  1,215 ± 0,019   s/op
-     */
-    private static List<List<String>> readCsvDataBigBuffer() {
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("people-2000000.csv"), 1024 * 1024 * 300)) {
-            String line;
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            throw new NoSuchElementException("Cannot read CSV file", e);
-        }
-        return records;
-    }
-
-    /**
-     * Benchmark      Mode  Cnt  Score   Error  Units
-     * CsvReader.run  avgt   50  1,201 ± 0,021   s/op
-     */
-    private static List<List<String>> readCsvDataBufferedOld() {
-        List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("people-2000000.csv"))) {
             String line;
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            throw new NoSuchElementException("Cannot read CSV file", e);
-        }
-        return records;
-    }
+                String birthDate = values[7];
 
-    /**
-     * Benchmark      Mode  Cnt  Score   Error  Units
-     * CsvReader.run  avgt   50  0,292 ± 0,007   s/op
-     */
-    private static List<String> readShortenedFile() {
-        List<String> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("people-2000000-shortened.csv"))) {
-            String line;
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(values[1]);
+                int month = Integer.parseInt(birthDate.substring(5, 7));
+                int day = Integer.parseInt(birthDate.substring(8));
+                int calenderIndex = (month - 1) * 31 + (day - 1);
+
+                personsPerBirthDay[calenderIndex]++;
+
+                if (maximumPartyFriends < personsPerBirthDay[calenderIndex]) {
+                    maximumPartyFriends = personsPerBirthDay[calenderIndex];
+                    winner = birthDate;
+                }
             }
         } catch (IOException e) {
             throw new NoSuchElementException("Cannot read CSV file", e);
         }
-        return records;
+        return winner.substring(5);
     }
 }
